e5830facc6ed2144fda5849b82e0de0346ec1037
irishjars <s.rishiraj6127@gmail.com>
2020-08-04 00:59:45 -0700

    First commit: req, tweepy code and handpicked gold, jerry, gold!

---
+++ ViceInd_bot.py
@@ -0,0 +1,35 @@
+import tweepy
+import logging
+import os
+import time
+
+logger = logging.getLogger()
+
+def create_api():
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    print("API created")
+    return api
+
+if __name__=="__main__":
+    with open("Gold_ViceInd_tweets.txt") as f:
+        api = create_api()
+        for line in f:
+            print(line)
+            api.update_status(line)
+            time.sleep(302400)
+
+
