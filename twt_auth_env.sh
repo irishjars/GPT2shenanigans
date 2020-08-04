e156832a261c3fa597feb2eb7fe465befa8aab79
irishjars <s.rishiraj6127@gmail.com>
2020-08-04 01:04:24 -0700

    readme + dummy shell script to set auth env variables

---
+++ twt_auth_env.sh
@@ -0,0 +1,4 @@
+  export CONSUMER_KEY="Your consumer key"
+  export CONSUMER_SECRET="Your consumer secret key"
+  export ACCESS_TOKEN="Your access token"
+  export ACCESS_TOKEN_SECRET="Your secret access token"
